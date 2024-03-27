dbb76bf703cd61e4bbc2119c16acada5fcd517eb
Fernando Nicolás <fersanti2896@gmail.com>
2024-03-27 11:48:29 -0600

    Se agrega componente de Items.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-
+import { menuItems } from './database/db';
+import { MenuItem } from './components/MenuItem';
 
 function App() {
+  console.log(menuItems)
 
   return (
     <>
       <header className="bg-green-500 py-7">
         <h1 className="text-center text-4xl font-black">Ticket de Cuenta</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+          {
+            menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+              />
+            ))
+          }
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,8 @@
+
+export const MenuItem = () => {
+    return (
+        <>
+            <h1>Hola</h1>
+        </>
+    )
+}
