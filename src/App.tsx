c19c705a953fc1a0b39fa8720c93e0d74546b8d8
Fernando Nicolás <fersanti2896@gmail.com>
2024-03-27 02:48:27 -0600

    Creando header.

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ function App() {
 
   return (
     <>
-      <h1 className="text-2xl">Hola mundo</h1>
-      <p>Hola mundo 2</p>
+      <header className="bg-green-500 py-7">
+        <h1 className="text-center text-4xl font-black">Ticket de Cuenta</h1>
+      </header>
     </>
   )
 }
